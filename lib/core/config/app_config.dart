4006120914531298b2006d1359d57c26028c7cf8
tuthanhnguyen <tuthanhnguyen67@gmail.com>
2026-09-07 18:37:38 +0700

    chore(release): bump version to v1.0.5

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,8 +1,8 @@
 /// Centralized application configuration and metadata.
 class AppConfig {
   static const String appName = 'Flanki';
-  static const String version = '1.0.4';
-  static const int buildNumber = 5;
+  static const String version = '1.0.5';
+  static const int buildNumber = 6;
 
   /// Full version string (e.g. "1.0.0+1")
   static const String fullVersion = '$version+$buildNumber';
--- packaging/windows/inno_setup.iss
+++ packaging/windows/inno_setup.iss
@@ -1,5 +1,5 @@
 #ifndef MyAppVersion
-#define MyAppVersion "1.0.4"
+#define MyAppVersion "1.0.5"
 #endif
 
 [Setup]
